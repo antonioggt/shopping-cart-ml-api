1b472e7f74ae217c8fda8515bafe8ecaf58705ed
antonioggt <antonioggt85@gmail.com>
2022-10-13 23:54:33 -0300

    entrega o requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,27 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('se fetchItem é uma função', async () => {
+    expect.assertions(1);
+    expect(await fetchItem).toBeInstanceOf(Function);
+  });
+  it('Deve chamar a função fetch', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527')
+
+    expect(global.fetch).toHaveBeenCalled();
+  });
+  it('Deve chamar a função fetch com o valor adequado', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527')
+
+    expect(global.fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it('Se dado determinado parâmetro, retorna a estrutura de dados correta', async () => {
+    expect.assertions(1);
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it("Se ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: 'You must provide an url'", async () => {
+    await expect(fetchItem()).rejects.toThrow('You must provide an url');
+  });
 });
